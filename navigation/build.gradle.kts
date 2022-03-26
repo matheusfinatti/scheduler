05bf8fbc35acfd523c9ce12044472d8eb5513e7e
Matheus Fernando Finatti <matheus.finatti@superawesome.com>
2022-03-26 12:40:53 +0000

    add navigation module

---
+++ navigation/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id(GradlePlugins.android.library)
+    id(GradlePlugins.kotlin.android)
+}
+
+android {
+    compileSdk = Versions.compileSdk
+
+    defaultConfig {
+        minSdk = Versions.minSdk
+        targetSdk = Versions.targetSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        debug {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.compose
+    }
+}
+
+dependencies {
+    implementation(project(Projects.core))
+
+    implementation(Dependencies.android.core)
+    implementation(Dependencies.android.navigation.compose)
+
+    testImplementation(Dependencies.test.jUnit)
+}
